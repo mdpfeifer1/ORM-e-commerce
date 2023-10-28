7687031a95765163d441017b2efa786cf9c04231
mdpfeifer1 <mikelaidenpfeifer@gmail.com>
2023-10-27 20:20:23 -0500

    made connections in process of testing

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+// const sequelize = require('./connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +12,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
+// sequelize.sync({ force: false }).then(() => {
+//   console.log('All models synced to the database');
+// }).catch((error) => {
+//   console.error('Error syncing models to the database:', error);
+// });
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
