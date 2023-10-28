d98e8f6f300057ebf9415d4f395504044e853c67
mdpfeifer1 <mikelaidenpfeifer@gmail.com>
2023-10-27 19:26:06 -0500

    API/PRODUCT-ROUTES.JS updated and completed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,27 +4,57 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category, 
+          attributes: ['id', 'category_name'] 
+        },
+        {
+          model: Tag,
+          attributes: ['id', 'tag_name'] 
+        }
+      ]
+    });
+    
+    res.status(200).json(products); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' }); 
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
+router.get('/:id', async (req, res) => {
+  try {
+    const product = await Product.findByPk(req.params.id, {
+      include: [
+        {
+          model: Category, 
+          attributes: ['id', 'category_name'] 
+        },
+        {
+          model: Tag, 
+          attributes: ['id', 'tag_name']
+        }
+      ]
+    });
 
+    if (!product) {
+      return res.status(404).json({ message: "No product found with that id!" });
+    }
+
+    res.status(200).json(product); 
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -92,8 +122,21 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedProductCount = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (deletedProductCount === 0) {
+      return res.status(404).json({ message: "No product found with that id!" });
+    }
+    res.status(200).json({ message: "Product deleted successfully!" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
 });
 
 module.exports = router;
